f28a8769f5d67c91ba29b4b00e762ac4af5aa1dc
asus <de>
2020-09-22 14:14:43 +0300

    Added test for Input

---
+++ src/test/java/InputTest.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+
+public class InputTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resourses/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(false);
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void elementsTest() {
+        driver.get("http://the-internet.herokuapp.com/inputs");
+        WebElement input = driver.findElement(By.tagName("input"));
+        input.sendKeys("5");
+        input.sendKeys(Keys.ARROW_UP);
+        assertEquals(input.getAttribute("value"), "6");
+        input.sendKeys(Keys.ARROW_DOWN);
+        assertEquals(input.getAttribute("value"), "5");
+
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
+//Inputs - Проверить на возможность ввести различные цифровые и нецифровые значения,
+// используя Keys.ARROW_UP  И Keys.ARROW_DOWN
